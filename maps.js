17670c871d6a5a06287633f86a4c144a18a89a1e
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2020-02-04 17:48:58 +0100

    modified the map to be able to have an itinarary, added Nunito font, made the menu clickable + smooth scroll

---
+++ maps.js
@@ -0,0 +1,16 @@
+// <script>
+// // Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   var gym = {lat: 50.433571, lng: 4.526110};
+//   // The map, centered at Uluru
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 15, center: gym});
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: gym, map: map});
+// }
+// </script>
+
+// <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDBytZVwTAyTtQ3N5HqXXRkDUYHE4Mo0rI&callback=initMap"
+//   type="text/javascript">
+// </script>
